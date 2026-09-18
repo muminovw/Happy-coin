949b118fa8594a251afe8f078583bfba75ae3c60
muminovw <mmuminovmustafo@gmail.com>
2026-09-18 17:40:24 +0500

    update

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../lib/SupabaseClient';
 
 // =====================================================
 // AUTH STATE
